88dc25c2f76ca8b23b640952f1714ad92cbcba77
Kimoi, John Gechure <John.Kimoi@equitybank.co.ke>
2024-08-13 11:41:25 +0300

    Add project files.

---
+++ mtnmomotest/Controllers/ReconcileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace mtnmomotest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReconcileController : ControllerBase
+    {
+        ILogger<ReconcileController> logger;
+
+        public ReconcileController(ILogger<ReconcileController> logger)
+        {
+                this.logger = logger;
+        }
+
+        [HttpPost]
+        [Route("/Reconcile")]
+        public IActionResult ReconcileCallback(JsonDocument obj)
+        {
+
+            string data  = obj.RootElement.ToString();
+
+            logger.LogInformation($"This is the data from the callback {data}");
+
+            return Ok(obj);
+        }
+    }
+}
